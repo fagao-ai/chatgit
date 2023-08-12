bd3cb54b4b71ddf353a0a6df488caf85993b453e
hezz <hezhaozhaog@gmail.com>
2023-08-12 15:51:07 +0800

    test pre commit

--- chatgit/crawl_git/github.py
+++ chatgit/crawl_git/github.py
@@ -4,26 +4,28 @@ import requests
 # https://docs.github.com/
 
 proxies = {
-    'http': 'http://127.0.0.1:1080',
+    "http": "http://127.0.0.1:1080",
 }
 
 # GitHub API 搜索的 URL
-search_url = 'https://api.github.com/search/repositories?q=stars:>100'
+search_url = "https://api.github.com/search/repositories?q=stars:>100"
 
 # 设置请求头，包括用户代理和认证
 
 # 发送 API 请求
-response = requests.get(search_url,
-                        # headers=headers,
-                        proxies=proxies)
+response = requests.get(
+    search_url,
+    # headers=headers,
+    proxies=proxies,
+)
 
 # 解析 JSON 响应
 data = response.json()
 
 # 打印搜索结果
-for item in data['items']:
+for item in data["items"]:
     print(f"Repository: {item['name']}")
     print(f"Stars: {item['stargazers_count']}")
     print(f"Description: {item['description']}\n")
-aa = "aaaa"*   100
+aa = "aaaa" * 100
 print(aa)
