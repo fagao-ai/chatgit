be69936b57d67e5054416da61359f959e1a086d8
hezhaozhao <hezhaozhaog@gmail.com>
2023-08-17 21:41:31 +0800

    add get github readme code

--- chatgit/crawl_git/github.py
+++ chatgit/crawl_git/github.py
@@ -1,31 +1,55 @@
-import requests
+import urllib.request
+import json
+import os
+import time
+from playwright.sync_api import sync_playwright
+import random
 
-# 替换为你的访问令牌
-# https://docs.github.com/
 
-proxies = {
-    "http": "http://127.0.0.1:1080",
+n = 5  # number of fetched READMEs
+url = 'https://api.github.com/search/repositories?q=stars:%3E500&sort=stars'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
+request = urllib.request.Request(url, headers=headers)
+response = urllib.request.urlopen(request)
+page = response.read().decode()
+api_json = json.loads(page)
 
-# GitHub API 搜索的 URL
-search_url = "https://api.github.com/search/repositories?q=stars:>100"
+repos = api_json['items'][:n]
 
-# 设置请求头，包括用户代理和认证
+p = sync_playwright().start()
 
-# 发送 API 请求
-response = requests.get(
-    search_url,
-    # headers=headers,
-    proxies=proxies,
-)
 
-# 解析 JSON 响应
-data = response.json()
+browsers = [browser_type.launch() for browser_type in [p.chromium]]
+for repo in repos:
+    full_name = repo['full_name']
+    contents_url = repo['url'] + '/contents'
+    request = urllib.request.Request(contents_url, headers=headers)
+    response = urllib.request.urlopen(request)
+    page = response.read().decode()
+    contents_json = json.loads(page)
+    readme_url = [file['download_url'] for file in contents_json if file['name'].lower() == 'readme.md'][0]
+    # download readme contents
+    try:
+        browser = random.choice(browsers)
+        page = browser.new_page()
+        page.goto(readme_url)
+        print(page.content())
+        browser.close()
+    except urllib.error.HTTPError as error:
+        print(error)
+        continue
 
-# 打印搜索结果
-for item in data["items"]:
-    print(f"Repository: {item['name']}")
-    print(f"Stars: {item['stargazers_count']}")
-    print(f"Description: {item['description']}\n")
-aa = "aaaa" * 100
-print(aa)
+    # create folder named after repo's name and save readme.md there
+    try:
+        os.mkdir(repo['name'])
+    except OSError as error:
+        print(error)
+    with open(repo['name'] + '/README.md', 'wb') as f:
+        # f.write(resp.content)
+        print('ok')
+
+    # only 10 requests per min for unauthenticated requests
+    if n >= 9:  # n + 1 initial request
+        time.sleep(6)
