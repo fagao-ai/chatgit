bba4677ad26ddd4fef24fe4f1511f13e307c8bb6
zhangluguang <z397503810@gmail.com>
2025-03-08 00:39:08 +0800

    feat: ✨ update UI

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
+    // tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
