62b12f0329a43d3df72b466a01fab2c1ac1b9296
sube <z397503810@gmail.com>
2025-03-19 00:22:08 +0800

    refactor: migrations

--- tasks/db.py
+++ tasks/db.py
@@ -7,7 +7,7 @@ def revision(ctx: Context, comments: str):
     comments = "_".join(comments.split(" "))
     if comments:
         ctx.run(
-            f'export PYTHONPATH=$PYTHONPATH:$PWD && cd chatgit/migrations && alembic revision -m "{comments}" --rev-id "`date +%Y%m%d_%H%M%S`" '
+            f'alembic -c migrations/alembic.ini revision -m "{comments}" --rev-id "`date +%Y%m%d_%H%M%S`" '
         )  # noqa: E501
     else:
         print("Please press migration comments.")
@@ -16,10 +16,10 @@ def revision(ctx: Context, comments: str):
 @task
 def migrate(ctx: Context):
     command = "upgrade head"
-    ctx.run(f"cd chatgit/migrations && alembic {command}")
+    ctx.run(f"alembic -c migrations/alembic.ini {command}")
 
 
 @task
 def downgrade(ctx: Context, vision: str = "-1"):
     command = f"downgrade {vision}"
-    ctx.run(f"cd chatgit/migrations && alembic {command}")
+    ctx.run(f"alembic -c migrations/alembic.ini {command}")
