f5f547d7c881256a5444ef418abd5a9f24339b84
sube <z397503810@gmail.com>
2025-03-21 00:48:09 +0800

    feat: :sparkles: add UI Framework

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import Aura from '@primeuix/themes/aura'
 import './assets/styles.css'
 import App from './App.vue'
 // import router from './router'
 import 'github-markdown-css/github-markdown.css'
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+  ripple: true,
+})
+app.use(ToastService)
 // app.use(router)
 
 app.mount('#app')
